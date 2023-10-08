4bdf1c2880e22338cbd61bd3832090c691ea3bd2
Afrie Irham <afrieirham.work@gmail.com>
2023-10-08 12:37:50 +0800

    add clerk and setup linkRouter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
-
+import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
-import "~/styles/globals.css";
-
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
