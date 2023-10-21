3613f27f98079d0241b26cbc888568e2248df872
Afrie Irham <afrieirham.work@gmail.com>
2023-10-21 22:06:39 +0800

    fix redirect not working

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,33 +1,12 @@
-import type {
-  GetStaticPaths,
-  GetStaticProps,
-  InferGetStaticPropsType,
-} from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { db } from "~/server/db";
 import { api } from "~/utils/api";
 
-export const getStaticProps: GetStaticProps<{ slug: string }> = (context) => {
-  const slug = context?.params?.slug as string;
-  return { props: { slug } };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const links = await db.link.findMany();
-  const paths = links.map((link) => ({ params: { slug: link.slug } }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-function RedirectPage({
-  slug,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+function RedirectPage() {
   const router = useRouter();
-  const { data } = api.link.getLinkBySlug.useQuery({ slug });
+  const { data } = api.link.getLinkBySlug.useQuery({
+    slug: String(router.query.slug),
+  });
   const { mutate } = api.link.updateNextPhone.useMutation();
 
   useEffect(() => {
