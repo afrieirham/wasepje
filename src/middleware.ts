2db15d0984a8568deb12d048ab94b8f1a40a34b0
Afrie Irham <afrieirham.work@gmail.com>
2023-10-23 23:03:39 +0800

    make create links accessible by public

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,12 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/:slug",
+    "/api/trpc/link.getLinkBySlug",
+    "/api/trpc/link.updateNextPhone",
+  ],
 });
 
 export const config = {
