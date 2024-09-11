898bef332a3f7c505c7057317cc9f2ea073a19c1
Afrie Irham <afrieirham.work@gmail.com>
2024-09-11 16:20:09 +0800

    add ads.txt, script tag, meta tag for adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,13 @@ export default function Document() {
           src="https://beamanalytics.b-cdn.net/beam.min.js"
           data-token={process.env.NEXT_PUBLIC_BEAM_ANALYTICS_DATA_TOKEN}
           async
-        ></script>
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5654598090665312"
+          crossOrigin="anonymous"
+        />
+        <meta name="google-adsense-account" content="ca-pub-5654598090665312" />
       </Head>
       <body>
         <Main />
