5c6e42d4a2a30732204acb2adaf75b70a0ef467c
Afrie Irham <afrieirham.work@gmail.com>
2024-09-13 01:27:32 +0800

    remove fetching user country

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,7 +2,6 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { UAParser } from "ua-parser-js";
 
-import { countries } from "~/constants/countries";
 import { api } from "~/utils/api";
 
 function RedirectPage() {
@@ -13,39 +12,33 @@ function RedirectPage() {
   const { mutate } = api.link.updateNextPhone.useMutation();
 
   useEffect(() => {
-    fetch("https://api.country.is")
-      .then((res) => res.json())
-      .then(({ country }: { ip: string; country: string }) => {
-        if (data) {
-          if (data.banned) {
-            void router.push("/link-banned");
-            return;
-          }
+    if (data) {
+      if (data.banned) {
+        void router.push("/link-banned");
+        return;
+      }
 
-          const phoneNumber = data.phones.at(Number(data.nextPhone))?.number;
-          let url = `https://wa.me/${phoneNumber}`;
+      const phoneNumber = data.phones.at(Number(data.nextPhone))?.number;
+      let url = `https://wa.me/${phoneNumber}`;
 
-          if (data?.message) {
-            url = url + `?text=${encodeURI(String(data.message))}`;
-          }
+      if (data?.message) {
+        url = url + `?text=${encodeURI(String(data.message))}`;
+      }
 
-          const userCountry = countries.get(country.toUpperCase());
+      const ua = UAParser(window.navigator.userAgent);
+      const metadata = {
+        browser: ua?.browser.name ?? "(unknown)",
+        country: "(unknown)",
+        continent: "(unknown)",
+        device: ua?.device.type ?? "Desktop",
+        os: ua?.os.name ?? "(unknown)",
+        referrer: document.referrer || "(direct)",
+      };
 
-          const ua = UAParser(window.navigator.userAgent);
-          const metadata = {
-            browser: ua?.browser.name ?? "(unknown)",
-            country: userCountry ? userCountry.name : "(unknown)",
-            continent: userCountry ? userCountry.continent : "(unknown)",
-            device: ua?.device.type ?? "Desktop",
-            os: ua?.os.name ?? "(unknown)",
-            referrer: document.referrer || "(direct)",
-          };
-          mutate({ id: data.id, metadata });
+      mutate({ id: data.id, metadata });
 
-          void router.push(url);
-        }
-      })
-      .catch((error) => console.log(error));
+      void router.push(url);
+    }
   }, [data, mutate, router]);
 
   return null;
