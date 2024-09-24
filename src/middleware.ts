0f47831d161fe5cd6f7b27c5be2590c8f8dd76d9
Afrie Irham <afrieirham.work@gmail.com>
2024-09-24 18:22:59 +0800

    bypass for all

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,9 @@ import { NextResponse } from "next/server";
 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-import { checkReserved } from "./server/api/routers/link";
-
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware(async (auth, request) => {
+export default clerkMiddleware((auth, request) => {
   const pathname = request.nextUrl.pathname;
   const clerkId = auth().userId;
 
@@ -29,38 +27,6 @@ export default clerkMiddleware(async (auth, request) => {
     return NextResponse.redirect(new URL("/user-banned", request.url));
   }
   // end handle banned users
-
-  // start check link plan
-  const isOneSlash = pathname.split("/").length === 2;
-  const cleanPathname = pathname.replace("/", "");
-  const isReserved = checkReserved(cleanPathname);
-
-  if (isOneSlash && !isReserved) {
-    const res = await fetch(new URL("/api/plan-check", request.url), {
-      method: "post",
-      body: cleanPathname,
-    });
-
-    const { plan } = (await res.json()) as { plan: "free" | "pro" };
-
-    if (plan === "free") {
-      return NextResponse.rewrite(
-        new URL(`/free/${cleanPathname}`, request.url),
-      );
-    }
-
-    if (plan) {
-      return NextResponse.rewrite(
-        new URL(`/skip/${cleanPathname}`, request.url),
-      );
-    }
-
-    // TODO: add link not found page
-    return NextResponse.next();
-  }
-  // end check link plan
-
-  return NextResponse.next();
 });
 
 export const config = {
