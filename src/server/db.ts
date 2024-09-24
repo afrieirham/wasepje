2a80abc1890d0604f0689cc7fe5985f26cb59b9a
Afrie Irham <afrieirham.work@gmail.com>
2024-09-25 00:01:33 +0800

    only log errors

--- src/server/db.ts
+++ src/server/db.ts
@@ -9,8 +9,7 @@ const globalForPrisma = globalThis as unknown as {
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: ["error"],
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
